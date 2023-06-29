ef4a10e7a1c2b7205ddf4ad1db8f8da58c9687d4
tasajos <tasajos@gmail.com>
2023-06-29 12:37:01 -0400

    updates

--- imhotep/src/app/app.module.ts
+++ imhotep/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +26,9 @@ import { ProcessmakerComponent } from './Components/processmaker/processmaker.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
